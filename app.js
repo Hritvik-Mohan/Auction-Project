2f7a38db4befcea41c3a8e23c3030240d4ae27b6
Hritvik Mohan <hritvik5005@gmail.com>
2022-02-17 19:50:49 +0530

    Added basic login and sign up form in views. And removed the unnecessary code from footer.ejs

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login ', (req, res) => {
+})
+
+app.get('/signup', (req, res) => {
+  res.render('signup.ejs')
+})
+
+app.post('/signup', (req, res) => {
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
