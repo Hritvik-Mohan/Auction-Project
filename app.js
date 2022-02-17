02a950e1868244b0f6f3f207bebb56d27d280584
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2022-02-17 21:06:47 +0530

    added userDB

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require("mongoose");
 const path = require('path');
+const { stringify } = require('querystring');
 
 const app = express();
 const PORT = 3000;
-const userDB = process.env.MONGO_CONNECTION;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
+
+// Database connectivity
+const userDB = process.env.MONGO_CONNECTION;
+
+mongoose.connect(userDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("user", userSchema);
 
 app.route('/')
 .get((req, res)=>{
