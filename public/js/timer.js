8ef6010c632e1a0f58bf8755230fcd727e751efe
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2022-04-15 15:44:37 +0530

    Made start time and duration accessible in js files

---
+++ public/js/timer.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log(startTime);
+console.log(duration);
+
+const durationInSeconds = duration * 24 * 60 * 60;
+var date = new Date(startTime);
+var seconds = date.getTime() / 1000;
+
+
+// console.log(seconds);
+// // time remaining in seconds
+// console.log()
