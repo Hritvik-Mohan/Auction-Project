cc554ae9e3bcee7d26bddefb96b1891634f94f5f
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2022-06-06 16:23:58 +0530

    [UPDATE] Timer is now visible for all the products on product page

--- public/js/product/indexPage.js
+++ public/js/product/indexPage.js
@@ -1,7 +1,62 @@
 "use strict";
 console.log('productPage script loaded...'); 
+const today = new Date().toISOString();
 let prodArr = JSON.parse(products);
 
-prodArr.forEach((product)=>{
-    console.log(product.startTime, typeof product.startTime)
-});
+const initTimeSeconds = (product) => {
+    const startTimeInSeconds = new Date(product.startTime).getTime() / 1000;
+    const durationInSeconds = product.duration * 24 * 60 * 60;
+    const endTimeInSeconds = startTimeInSeconds + durationInSeconds;
+
+    // Get difference between current date and endTime in seconds.
+    const differenceInSeconds = endTimeInSeconds - new Date(today).getTime() / 1000;
+    return Math.floor(differenceInSeconds);
+}
+
+// Calculate the time remaining.
+const timeRemaining = (product) => {
+    const days = Math.floor(product.timeRemainingInSeconds / (24 * 60 * 60));
+    const hours = Math.floor(product.timeRemainingInSeconds % (24 * 60 * 60) / (60 * 60));
+    const minutes = Math.floor(product.timeRemainingInSeconds % (24 * 60 * 60) % (60 * 60) / 60);
+    const seconds = Math.floor(product.timeRemainingInSeconds % (24 * 60 * 60) % (60 * 60) % 60);
+
+    const labelTimer = document.getElementById(`timer-${product._id}`);
+    labelTimer.textContent = `${days}D ${hours}H ${minutes}M ${seconds}S`;
+
+    if (product.timeRemainingInSeconds === 0) {
+        clearInterval(product.timer);
+        labelTimer.textContent = `CLOSED`;
+    } 
+
+    product.timeRemainingInSeconds--;
+}
+
+const main = () => {
+    // console.log(productTimers);
+    // createTimer();
+    prodArr.forEach(product => {
+        const startTime = new Date(product.startTime).toISOString();
+        const startTimeInSeconds = new Date(startTime).getTime() / 1000;
+        const endTimeInSeconds = startTimeInSeconds + (product.duration * 24 * 60 * 60);
+        let endTime = new Date(endTimeInSeconds * 1000);
+        endTime =  endTime.toISOString();
+
+        if (today >= startTime && today <= endTime) {
+            console.log('Auction is running...');
+            product.timeRemainingInSeconds = initTimeSeconds(product);
+            const MILLISECONDS_IN_A_SECOND = 1000;
+            product.timer = setInterval(()=>{
+                timeRemaining(product);
+            }, MILLISECONDS_IN_A_SECOND);
+        } else if (today < startTime) {
+            const labelTimer = document.getElementById(`timer-${product._id}`);
+            labelTimer.textContent = `OFFLINE`;
+        } else {
+            const labelTimer = document.getElementById(`timer-${product._id}`);
+            labelTimer.textContent = `CLOSED`;
+        }
+
+    });
+}
+
+main();
