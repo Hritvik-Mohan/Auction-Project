e57b24b01708c8d184b555715cc5e68ae9b8aed6
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2022-06-08 20:21:09 +0530

    Added backend validation for registeration form

---
+++ utils/misc.functions.js
@@ -0,0 +1,15 @@
+/**
+ * @description - This function returns the date eight years ago from now.
+ * 
+ * @returns String - Date in the format of YYYY-MM-DD
+ */
+const dateEighteenYearsAgo = () => {
+    const today = new Date();
+    const eighteenYearsAgo = new Date(today.setFullYear(today.getFullYear() - 18));
+    const year = eighteenYearsAgo.getFullYear();
+    const month = (eighteenYearsAgo.getMonth()+"").padStart(2, "0");
+    const date = (eighteenYearsAgo.getDate()+"").padStart(2, "0");
+    return `${year}-${month}-${date}`;
+}
+
+module.exports = { dateEighteenYearsAgo };
