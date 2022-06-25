6e067eb07e95fac60574f962c951dd2483c699e7
Hritvik Mohan <hritvik5005@gmail.com>
2022-06-25 18:42:28 +0530

    Add product images slider.

--- public/js/product/productPage.js
+++ public/js/product/productPage.js
@@ -157,3 +157,25 @@ const main = () => {
     }
 }
 main();
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
