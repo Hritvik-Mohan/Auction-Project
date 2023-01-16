538708973dc7f3585b79072a45d9150be95eb519
Harshit Kr Vishwakarma <vharshitkr01gmail.com>
2023-01-16 21:01:22 +0530

    fix ui

--- samples/populatedb.js
+++ samples/populatedb.js
@@ -6,59 +6,63 @@ let testUserId;
 let product;
 
 const populateDb = async () => {
-    try {
-        await connectdb("mongodb://localhost:27017/auctionDB");
-        const user = new User({
-            firstName: "John",
-            lastName: "Doe",
-            role: 'ROLE_USER',
-            email: "somedummyemail@gmail.com", // should be unique
-            password: "12345678",
-            phoneNumber: "7654897671", // should be unique
-            verified: true,
-            dob: "2000-01-01"
-        });
+	try {
+		await connectdb("mongodb://localhost:27017/auctionDB");
+		const user = new User({
+			firstName: "John",
+			lastName: "Doe",
+			role: "ROLE_USER",
+			email: "somedummyemail@gmail.com", // should be unique
+			password: "12345678",
+			phoneNumber: "7654897671", // should be unique
+			verified: true,
+			dob: "2000-01-01",
+		});
 
-        await user.save();
-        testUserId = user._id;
+		await user.save();
+		testUserId = user._id;
 
-        const images = [{
-            path:"https://res.cloudinary.com/dqw9l9khp/image/upload/v1656514108/AuctionApp/book_l3u5mj.jpg",
-            filename:"AuctionApp/12fer45tf"
-        }];
+		const images = [
+			{
+				path: "https://res.cloudinary.com/dqw9l9khp/image/upload/v1656514108/AuctionApp/book_l3u5mj.jpg",
+				filename: "AuctionApp/12fer45tf",
+			},
+		];
 
-        for(let i = 0; i < 18; i++) {
-            product = new Product({
-                title: "You are not so smart.",
-                description: "You are not so smart. - David Macreny",
-                basePrice: 100,
-                images,
-                startTime: new Date(),
-                duration: 10,
-                category: "books",
-                auctionStatus: true,
-                user: testUserId
-            });
-        
-            await product.save();
-            console.log(`Product ${i+1} of ${20} saved`);
-        }
-    } catch(e){
-        console.log(e);
-    }
+		for (let i = 0; i < 18; i++) {
+			product = new Product({
+				title: "You are not so smart.",
+				description: "You are not so smart. - David Macreny",
+				basePrice: 100,
+				images,
+				startTime: new Date(),
+				duration: 10,
+				category: "books",
+				auctionStatus: true,
+				user: testUserId,
+			});
+
+			await product.save();
+			console.log(`Product ${i + 1} of ${20} saved`);
+		}
+		process.exit(0);
+	} catch (e) {
+		console.log(e);
+	}
 };
 
 // populateDb();
 
 const pruneDb = async () => {
-    try {
-        await connectdb("mongodb://localhost:27017/auctionDB");
-        await Product.deleteMany({});
-        await User.deleteMany({});
-        console.log("All users and products deleted");
-    } catch (e){
-        console.log(e);
-    }
-}
+	try {
+		await connectdb("mongodb://localhost:27017/auctionDB");
+		await Product.deleteMany({});
+		await User.deleteMany({});
+		console.log("All users and products deleted");
+		process.exit(0);
+	} catch (e) {
+		console.log(e);
+	}
+};
 
 // pruneDb();
